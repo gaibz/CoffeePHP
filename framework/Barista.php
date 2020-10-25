af804adb8808724399568d877371de73bd09b5dc
Herlangga Sefani <herlangga.sefani@gmail.com>
2020-10-25 11:42:37 +0700

    Initial Commit

---
+++ framework/Barista.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace CoffeePHP;
+
+
+class Barista
+{
+
+    /**
+     * Brew CoffeePHP
+     * This is a startup script for initializing this framework
+     *
+     * @return $this
+     */
+    public function brew() : self
+    {
+        echo "Under Development";
+        return $this;
+    }
+
+}
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require(__DIR__ . "/vendor/autoload.php");
+
+$barista = new \CoffeePHP\Barista();
+$barista->brew();
